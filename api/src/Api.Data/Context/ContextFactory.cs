63fcb5be6d6297c184fe9466d7be200dbb3ff84b
uigormarshall <40609362+uigormarshall@users.noreply.github.com>
2022-06-02 09:41:16 -0300

    feat: atualiza conectionString para usar o banco docker

--- api/src/Api.Data/Context/ContextFactory.cs
+++ api/src/Api.Data/Context/ContextFactory.cs
@@ -7,7 +7,7 @@ namespace Api.Data.Context
     {
         public MyContext CreateDbContext(string[] args)
         {
-            var conectionString = "Server=localhost;Port=3306;Database=MyDatabase;Uid=root;Pwd=passwords";
+            var conectionString = "Server=localhost;Port=3306;Database=api;Uid=root;Pwd=secret";
             var optionBuilder = new DbContextOptionsBuilder<MyContext>();
             optionBuilder.UseMySql(conectionString);
             return new MyContext(optionBuilder.Options);
