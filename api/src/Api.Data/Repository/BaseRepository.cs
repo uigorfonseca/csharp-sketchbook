d254bd9832b76ef8c7d24641483e44e31aa40188
uigormarshall <40609362+uigormarshall@users.noreply.github.com>
2022-06-06 15:09:17 -0300

    feat: adiciona BaseRepository

---
+++ api/src/Api.Data/Repository/BaseRepository.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Api.Data.Context;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Repository
+{
+    public class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly MyContext _context;
+        private DbSet<T> _dataset;
+        public BaseRepository(MyContext context)
+        {
+            _context = context;
+            _dataset = _context.Set<T>();
+        }
+        public Task<bool> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> InsertAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SelectAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> SelectAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
