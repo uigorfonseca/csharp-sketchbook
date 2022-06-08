6a23b1c7fc58e263d7b9d59c63cad809fe747d71
uigormarshall <40609362+uigormarshall@users.noreply.github.com>
2022-06-08 14:36:28 -0300

    feat: adiciona configuracao de context na DI

--- api/src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ api/src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,5 +1,9 @@
+using Api.Data.Context;
 using Api.Data.Repository;
 using Api.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Api.CrossCutting.DependencyInjection
@@ -8,6 +12,7 @@ namespace Api.CrossCutting.DependencyInjection
     {
         public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
         {
+            serviceCollection.AddDbContext<MyContext>(options => options.UseMySql("Server=localhost;Port=3306;Database=api;Uid=root;Pwd=secret"));
             serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
         }
     }
