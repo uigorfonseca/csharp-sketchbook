9e2c2ee64d35b009d74863b70500e73a59b85ed5
uigormarshall <40609362+uigormarshall@users.noreply.github.com>
2022-06-08 12:01:40 -0300

    feat: adiciona UserController

---
+++ api/src/Api.Application/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Application.Controllers
+{
+    [ApiController]
+    [Route("[api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetAll([FromServices] IUserService service)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                return Ok(await service.GetAll());
+            }
+            catch (ArgumentException exception)
+            {
+                return StatusCode((int)System.Net.HttpStatusCode.InternalServerError, exception.message);
+            }
+        }
+    }
+}
