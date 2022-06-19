dd4c3e8f2a06f582452d0f650e4c6bf67b742abe
uigorfonseca <ufonseca@brasiliano.com.br>
2022-06-19 09:51:16 -0300

    feat: adiciona IUserRepository

---
+++ api/src/Api.Domain/Repository/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Api.Domain.Interfaces;
+using Domain.Entities;
+
+namespace Domain.Repository
+{
+    public interface IUserRepository : IRepository<UserEntity>
+    {
+        Task<UserEntity> FindByEmail(string email);
+    }
+}
