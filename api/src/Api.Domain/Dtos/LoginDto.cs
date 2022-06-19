cd1a129466c3084d911a9e45801fbaca1e9b86a4
uigorfonseca <ufonseca@brasiliano.com.br>
2022-06-19 10:43:55 -0300

    feat: adiciona loginDto

---
+++ api/src/Api.Domain/Dtos/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Dtos
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "Email é um campo obrigatório.")]
+        [EmailAddress(ErrorMessage = "Email com formato inválido.")]
+        [StringLength(100, ErrorMessage = "Email deve ter no máximo {1} caracteres.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password é um campo obrigatório.")]
+        public string Password { get; set; }
+    }
+}
