d0c2c72a848ea1279cb2fcbf6a62516706fe5b99
uigorfonseca <ufonseca@brasiliano.com.br>
2022-06-19 10:39:13 -0300

    feat: adiciona injecao de depencia LoginService e UserRepository(implementation)

--- api/src/Api.CrossCutting/DependencyInjection/ConfigureService.cs
+++ api/src/Api.CrossCutting/DependencyInjection/ConfigureService.cs
@@ -1,6 +1,8 @@
 
+using Api.Domain.Interfaces.Services.Auth;
 using Api.Domain.Interfaces.Services.User;
 using Api.Service.Services;
+using Api.Service.Services.Auth;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Api.CrossCutting.DependencyInjection
@@ -10,6 +12,7 @@ namespace Api.CrossCutting.DependencyInjection
         public static void ConfigureDependenciesService(IServiceCollection serviceCollection)
         {
             serviceCollection.AddTransient<IUserService, UserService>();
+            serviceCollection.AddTransient<ILoginService, LoginService>();
         }
     }
 }
