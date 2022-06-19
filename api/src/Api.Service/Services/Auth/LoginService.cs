ecc99523cbc2bf64c9bc076aff6dc2248dafac12
uigorfonseca <ufonseca@brasiliano.com.br>
2022-06-19 10:25:39 -0300

    feat: adiciona implementação de ILoginService

---
+++ api/src/Api.Service/Services/Auth/LoginService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Api.Domain.Interfaces.Services.Auth;
+using Domain.Entities;
+using Domain.Repository;
+
+namespace Api.Service.Services.Auth
+{
+    public class LoginService : ILoginService
+    {
+        private IUserRepository _userRepository;
+
+        public LoginService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserEntity> FindByEmail(string email)
+        {
+            return await _userRepository.FindByEmail(email);
+        }
+    }
+}
