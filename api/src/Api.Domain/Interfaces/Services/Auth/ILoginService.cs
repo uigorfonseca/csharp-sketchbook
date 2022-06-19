d3e0f14eefe519f7feba848b6d6f6bf9f58cbd43
uigorfonseca <ufonseca@brasiliano.com.br>
2022-06-19 10:18:54 -0300

    feat: adiciona interface ILoginService

---
+++ api/src/Api.Domain/Interfaces/Services/Auth/ILoginService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Api.Domain.Interfaces.Services.Auth
+{
+    public interface ILoginService
+    {
+        Task<UserEntity> FindByEmail(string email);
+    }
+}
