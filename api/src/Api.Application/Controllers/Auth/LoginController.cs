cd9328d8223b82d9b620527e3bba9d0fa07fc7ee
uigorfonseca <ufonseca@brasiliano.com.br>
2022-06-19 10:35:05 -0300

    feat: adiciona implementacao de loginController

---
+++ api/src/Api.Application/Controllers/Auth/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Api.Domain.Interfaces.Services.Auth;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace application.Controllers.Auth
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private ILoginService _loginService;
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        public async Task<object> login([FromBody] UserEntity userLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            if (userLogin == null)  return BadRequest();
+
+            var user = await _loginService.FindByEmail(userLogin.Email);
+            if (user == null) return NotFound();
+            
+            return user;
+        }
+    }
+}
