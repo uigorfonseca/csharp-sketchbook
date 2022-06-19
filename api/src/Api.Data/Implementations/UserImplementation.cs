6f2bbbc2c617062d8bac381a6e759f8fafe1c50f
uigorfonseca <ufonseca@brasiliano.com.br>
2022-06-19 09:59:05 -0300

    feat: adiciona UserImplementation

---
+++ api/src/Api.Data/Implementations/UserImplementation.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Api.Data.Context;
+using Api.Data.Repository;
+using Domain.Entities;
+using Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Implementations
+{
+    public class UserImplementation : BaseRepository<UserEntity>, IUserRepository
+    {
+        private DbSet<UserEntity> _dataset;
+
+        public UserImplementation(MyContext context) : base(context)
+        {
+            _dataset = context.Set<UserEntity>();
+        }
+
+        public async Task<UserEntity> FindByEmail(string email)
+        {
+            return await _dataset.FirstOrDefaultAsync(user => user.Email == email);
+        }
+    }
+}
