8e78e97a1d798f30ad87bbf32a7ca7256cf6d3b0
uigormarshall <40609362+uigormarshall@users.noreply.github.com>
2022-07-23 13:12:44 -0300

    feat: implementa classe de banco para testes

---
+++ api/src/Api.Data.Test/DbTest.cs
@@ -0,0 +1,32 @@
+﻿using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.Data.Test;
+public class DbTest : IDisposable
+{
+    private string _databaseName = $"dbTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+    public ServiceProvider ServiceProvider { get; }
+
+    public DbTest()
+    {
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddDbContext<MyContext>(o =>
+                o.UseMySql($"Server=localhost;Port=3306;Database={_databaseName};Uid=root;Pwd=secret"),
+            ServiceLifetime.Transient);
+
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+        using (var context = ServiceProvider.GetService<MyContext>())
+        {
+            context?.Database.EnsureCreated();
+        }
+    }
+    public void Dispose()
+    {
+        using (var context = ServiceProvider.GetService<MyContext>())
+        {
+            context?.Database.EnsureDeleted();
+        }
+    }
+}
