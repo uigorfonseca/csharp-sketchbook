62260f41b5add8cbf78f94d8b8fd9f63717fff3c
uigormarshall <40609362+uigormarshall@users.noreply.github.com>
2022-07-26 23:08:52 -0300

    test: adiciona OrdenarDadoONumero451DeveRetornar541

--- desafios/resolucoes.test/OrdemCrescenteTest.cs
+++ desafios/resolucoes.test/OrdemCrescenteTest.cs
@@ -8,4 +8,12 @@ public class OrdemCrescenteTest
         var resultado = OrdemCrescente.Ordenar(0);
         Assert.True(resultado >= 0);
     }
+
+    [Fact]
+    public void OrdenarDadoONumero451DeveRetornar541()
+    {
+        var esperado = 541;
+        var resulto = OrdemCrescente.Ordenar(541);
+        Assert.Equal(esperado, resulto);
+    }
 }
