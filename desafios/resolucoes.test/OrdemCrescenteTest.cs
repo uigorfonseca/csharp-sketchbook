4c366ab735319e11188aec8456530814240b3d9f
uigormarshall <40609362+uigormarshall@users.noreply.github.com>
2022-07-26 23:05:43 -0300

    test: adiciona OrdemCrescenteDeveRetornarUmInteiroNaoNegativo

---
+++ desafios/resolucoes.test/OrdemCrescenteTest.cs
@@ -0,0 +1,11 @@
+﻿namespace resolucoes.test;
+
+public class OrdemCrescenteTest
+{
+    [Fact]
+    public void OrdemCrescenteDeveRetornarUmInteiroNaoNegativo()
+    {
+        var resultado = OrdemCrescente.Ordenar(0);
+        Assert.True(resultado >= 0);
+    }
+}
