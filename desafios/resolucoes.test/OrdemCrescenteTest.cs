9c7e596fec1e69797c09d208f1f53cade38bbd60
uigormarshall <40609362+uigormarshall@users.noreply.github.com>
2022-07-26 23:23:41 -0300

    test: adiciona OrdenarDeveRetornarResultadoIgualATeoria

--- desafios/resolucoes.test/OrdemCrescenteTest.cs
+++ desafios/resolucoes.test/OrdemCrescenteTest.cs
@@ -16,4 +16,14 @@ public class OrdemCrescenteTest
         var resulto = OrdemCrescente.Ordenar(541);
         Assert.Equal(esperado, resulto);
     }
+    
+    [Theory]
+    [InlineData(456 ,654)]
+    [InlineData(7489 ,9874)]
+    [InlineData(145263  ,654321)]
+    public void OrdenarDeveRetornarResultadoIgualATeoria(int entrada, int esperado)
+    {
+        var resultado = OrdemCrescente.Ordenar(entrada);
+        Assert.Equal(esperado, resultado);
+    }
 }
